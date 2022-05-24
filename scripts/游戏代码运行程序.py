b05c7bc10c62b4175b224a780c04c13331ec19eb
Rainbow <2180502841@qq.com>
2022-05-24 23:33:58 +0800

    Update 游戏代码运行程序.py

--- scripts/游戏代码运行程序.py
+++ scripts/游戏代码运行程序.py
@@ -8,7 +8,7 @@ from copy import deepcopy
 
 abs_path = os.path.dirname(os.path.abspath(__file__))
 os.chdir(abs_path)
-with open('scripts/game_path.py', encoding='utf-8-sig') as f:
+with open('scripts/game_path.py', encoding='utf-8') as f:
     game_path = f.read()
 with open(game_path, encoding='utf-8') as f:
     exec(f.read())
